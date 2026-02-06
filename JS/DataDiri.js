dbccbeeadb7a6840ab17b823162e86a8ae7051a1
Tony Wijaya <wijayatony249@gmail.com>
2026-02-06 16:42:01 +0700

    Firts commit

---
+++ JS/DataDiri.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tombolSapa = document.getElementById('btnSapa');
+
+    tombolSapa.addEventListener('click', () => {
+        alert("Halo! Saya Tony Wijaya, mahasiswa Informatika UNSIL. Terima kasih sudah berkunjung ke halaman saya!");
+    });
+});
